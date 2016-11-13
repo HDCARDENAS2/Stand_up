00c9974246f47f67353cdf1cdc232aa3037ab641
Raphael Lara <lara_d_kli@hotmail.com>
2016-11-12 19:35:55 -0500

    Login

---
+++ Stand_Up/JS/js_login.js
@@ -0,0 +1,36 @@
+
+/**
+ * @author	Raphael Lara
+ * @email	lara_d_kli@hotmail.com
+ * @date	07/11/2016
+ */
+
+function fn_validar_login(){
+	
+	var usuario = $("#user_name").val();
+	var password   = $("#user_pass").val();
+	
+	var mensaje = "";
+	
+	if(usuario == ""){
+		mensaje +="Ingrese un usuario.\n";
+	}
+	
+	if(password == ""){
+		mensaje +="Ingrese una contraseña.\n";
+	}
+	
+	if(mensaje == ""){
+        /** Peticion ajax */
+		var respuesta = Ajax('forma_login_usuario',
+				             '../Control/control_login.php');
+		if(respuesta != null){
+			if(respuesta.resultado == 1 ){
+				
+			}
+		}	
+	
+	}else{
+		alert(mensaje);
+	}
+}
